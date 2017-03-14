a37a6441a64374f5c3546e167e85307baf5acee4
Krista <krista.hapner@gmail.com>
2017-03-14 13:18:06 -0400

    Added database files / folder

---
+++ db/create_db.sql
@@ -0,0 +1,25 @@
+drop table if exists user;
+create table user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  foreign key (id) REFERENCES attendance (user_id)
+);
+
+
+drop table if exists meetings;
+create table meetings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  date TEXT,
+  foreign key (id) REFERENCES attendance (date_id)
+
+);
+
+drop table if exists attendance;
+CREATE TABLE attendance (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  date_id INTEGER
+
+);
