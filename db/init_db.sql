b04a24d9a4c66d0941de29dafef055c0e64a79ed
Krista <krista.hapner@gmail.com>
2017-04-11 18:47:28 -0400

    created basic map functionality (p2)

--- db/init_db.sql
+++ db/init_db.sql
@@ -2,7 +2,7 @@ INSERT INTO member(email, first_name, last_name,  phone_number, gender, birthday
 INSERT INTO member(email, first_name, last_name,  phone_number, gender, birthday, baptism_status, join_date, is_active ) values ('nysha@example.com', 'Nysha', 'Chen', '111-222-3333', 'F', 'May 2, 1990', 1, 'August 1, 2016', 1 );
 INSERT INTO member(email, first_name, last_name,  phone_number, gender, birthday, baptism_status, join_date, is_active ) values ('christine@example.com', 'Christine', 'Urban', '555-135-3245', 'F', 'May 13, 1993', 1, 'August 1, 2016', 0 );
 
-INSERT INTO homegroup(name, location, description, latitude, longitude) values ('TWEET', '236 W. Reade Ave \n Upland, IN 46989', 'Taylor Women Engaged in Engineering and Technology',40.4756, -85.4944);
+INSERT INTO homegroup(name, location, description, latitude, longitude) values ('TWEET', '236 W. Reade Ave Upland, IN 46989', 'Taylor Women Engaged in Engineering and Technology',40.4756, -85.4944);
 
 INSERT INTO homegroup(name, location, description, latitude, longitude) values ('Ball State CRU', '2000 W University Ave Muncie, IN 47306', 'Cru at Ball State is a movement of people passionate about knowing Jesus and telling His story by getting, giving, and multiplying the Gospel.',40.2061, -85.4070);
 
