43a22fe2867e95e7437bd1ae00253e22844efda7
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-01-05 16:25:05 -0500

    Update SQL to create the data model.
    - Remove CASCADE keyword
    - Order DROP and CREATE statements to avoid dependencies

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,86 +1,83 @@
+DROP TABLE IF EXISTS member_role;
+DROP TABLE IF EXISTS homegroup_leader;
+DROP TABLE IF EXISTS homegroup_member;
+DROP TABLE IF EXISTS attendance;
 
-drop  table if exists member CASCADE;
-create table member (
-  id SERIAL PRIMARY KEY ,
-  first_name TEXT,
-  last_name TEXT,
-  email TEXT UNIQUE,
-  phone_number TEXT,
-  gender TEXT,
-  birthday TEXT,
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS homegroup;
+DROP TABLE IF EXISTS meeting;
+
+CREATE TABLE member (
+  id             SERIAL PRIMARY KEY,
+  first_name     TEXT,
+  last_name      TEXT,
+  email          TEXT UNIQUE,
+  phone_number   TEXT,
+  gender         TEXT,
+  birthday       TEXT,
   baptism_status BOOLEAN,
   marital_status BOOLEAN,
-  join_date TEXT,
-  is_active BOOLEAN
-
+  join_date      TEXT,
+  is_active      BOOLEAN
 );
 
-drop table if exists member_role  CASCADE;
-create table member_role(
-  member_id INTEGER,
-  password TEXT,
-  role_id INTEGER,
-  foreign key (member_id) REFERENCES member(id),
-  foreign key (role_id) references role(id)
-);
-drop table if exists role CASCADE;
-create table role (
-  id SERIAL PRIMARY KEY ,
+CREATE TABLE role (
+  id   SERIAL PRIMARY KEY,
   role TEXT
 );
 
+CREATE TABLE member_role (
+  member_id INTEGER,
+  password  TEXT,
+  role_id   INTEGER,
+  FOREIGN KEY (member_id) REFERENCES member (id),
+  FOREIGN KEY (role_id) REFERENCES role (id)
+);
 
-
-
-drop table if exists homegroup CASCADE;
-create table homegroup (
-  id SERIAL PRIMARY KEY ,
-  name TEXT,
-  location TEXT,
+CREATE TABLE homegroup (
+  id          SERIAL PRIMARY KEY,
+  name        TEXT,
+  location    TEXT,
   description TEXT,
-  latitude REAL,
-  longitude REAL,
-  is_active BOOLEAN
+  latitude    REAL,
+  longitude   REAL,
+  is_active   BOOLEAN
 
 );
 
-drop table if exists homegroup_leader CASCADE;
-create table homegroup_leader (
-  member_id INTEGER,
+CREATE TABLE homegroup_leader (
+  member_id    INTEGER,
   homegroup_id INTEGER,
-  is_active boolean,
-  foreign key (member_id) REFERENCES member(id),
-  foreign key (homegroup_id) references homegroup(id),
+  is_active    BOOLEAN,
+  FOREIGN KEY (member_id) REFERENCES member (id),
+  FOREIGN KEY (homegroup_id) REFERENCES homegroup (id),
   PRIMARY KEY (member_id, homegroup_id)
 );
 
 
-drop table if exists homegroup_member CASCADE;
-create table homegroup_member (
+CREATE TABLE homegroup_member (
   homegroup_id INTEGER,
-  member_id INTEGER,
-  is_active BOOLEAN,
-  foreign key (member_id) REFERENCES member (id),
-  foreign key (homegroup_id) REFERENCES homegroup(id),
+  member_id    INTEGER,
+  is_active    BOOLEAN,
+  FOREIGN KEY (member_id) REFERENCES member (id),
+  FOREIGN KEY (homegroup_id) REFERENCES homegroup (id),
   PRIMARY KEY (homegroup_id, member_id)
 );
 
-
-
-drop table if exists meeting CASCADE;
-create table meeting (
-  id SERIAL PRIMARY KEY ,
+CREATE TABLE meeting (
+  id   SERIAL PRIMARY KEY,
   date TEXT,
   time TEXT
 
 );
-drop table if exists attendance CASCADE;
+
 CREATE TABLE attendance (
   homegroup_id INTEGER,
-  member_id INTEGER,
-  meeting_id INTEGER,
-  attendance BOOLEAN,
-  foreign key (member_id) REFERENCES member (id),
-  foreign key (meeting_id) REFERENCES meeting (id),
-  PRIMARY KEY(homegroup_id, member_id, meeting_id)
+  member_id    INTEGER,
+  meeting_id   INTEGER,
+  attendance   BOOLEAN,
+  FOREIGN KEY (member_id) REFERENCES member (id),
+  FOREIGN KEY (meeting_id) REFERENCES meeting (id),
+  PRIMARY KEY (homegroup_id, member_id, meeting_id)
 );
