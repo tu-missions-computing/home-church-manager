4a70a83357f3fac1768210dc8fc9a25d2ed821fc
Nysha Chen <nysha_chen@taylor.edu>
2018-01-09 13:36:41 -0500

    better looking homepage + homepage button now works!

--- static/main.js
+++ static/main.js
@@ -4,3 +4,18 @@ $(document).ready(function () {
         else $('.content').fadeIn('slow');
     });
 });
+
+
+// For smooth scrolling
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
